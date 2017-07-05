e6ff77f24b963cceb2e5e82214886b940f1e1ec9
Slawomir Siudek <siudeks@gmail.com>
2017-07-05 23:18:53 +0200

    Remove unused code

--- DanmakuNoKyojin/Controls/ControlManager.cs
+++ DanmakuNoKyojin/Controls/ControlManager.cs
@@ -1,27 +1,15 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
-using System;
 
 namespace DanmakuNoKyojin.Controls
 {
     public class ControlManager
     {
-        int selectedControl = 0;
-        bool acceptInput = true;
-
         static SpriteFont spriteFont;
 
         public static SpriteFont SpriteFont
         {
             get { return spriteFont; }
         }
-
-        public bool AcceptInput
-        {
-            get { return acceptInput; }
-            set { acceptInput = value; }
-        }
-
-        public event EventHandler FocusChanged;
 
         public ControlManager(SpriteFont spriteFont)
             : base()
