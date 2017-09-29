eeb31975edd652b95934be18b07ff91583498182
Slawomir Siudek <siudeks@gmail.com>
2017-09-29 23:02:38 +0200

    Add new tests, update references

---
+++ Danmaku.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Akka.TestKit.NUnit;
+using NUnit.Framework;
+
+namespace Danmaku
+{
+    [TestFixture]
+    public class BulletActorShould : TestKit
+    {
+        [Test]
+        public void aaa()
+        {
+
+        }
+    }
+}
